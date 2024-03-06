bc9f6724cf2c87f68ba4b46f0f8bca7644235cbe
bruno-gds <brunog.damascena@gmail.com>
2024-03-06 20:59:07 -0300

    implementando ReservarEstoqueUseCase

---
+++ src/main/java/com/grupo16/estoqueservice/usecase/ReservarEstoqueUseCase.java
@@ -0,0 +1,26 @@
+package com.grupo16.estoqueservice.usecase;
+
+import com.grupo16.estoqueservice.domain.Estoque;
+import com.grupo16.estoqueservice.gateway.EstoqueRepositoryGateway;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 06/03/2024
+ * Project Name: estoque-service
+ */
+
+@Service
+@AllArgsConstructor
+public class ReservarEstoqueUseCase {
+
+    private EstoqueRepositoryGateway estoqueRepositoryGateway;
+
+
+    public List<Estoque> reservar(List<Estoque> estoqueList) {
+        return estoqueRepositoryGateway.reservar(estoqueList);
+    }
+}
